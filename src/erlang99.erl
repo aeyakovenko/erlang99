6746a02e2bba4bf0a7896305b7a4e72840c67890
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-08-22 09:57:57 -0700

    update

--- src/erlang99.erl
+++ src/erlang99.erl
@@ -1,8 +1,16 @@
 -module(erlang99).
--export([last/1]).
+-export([last/1, penultimate/1]).
 
 last([H|[]]) -> H;
 last([_|T]) -> last(T).
 
+last_test() -> 
+  3 = last([1,2,3]).
+
 penultimate([H,_|[]]) -> H;
 penultimate([_|T]) -> penultimate(T).
+
+penultimate_test() -> 
+  2 = penultimate([1,2,3]).
+
+
