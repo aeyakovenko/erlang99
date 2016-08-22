94dd0fd6f2ea9d8930cbfa577e9cbec431b51a31
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-08-22 10:24:57 -0700

    initial

--- Makefile
+++ Makefile
@@ -1,8 +1,4 @@
-FILES=test/test.erl\
-			src/erlang99.erl
+FILES=src/erlang99.erl
 
-out/ok:$(FILES) Makefile
-	mkdir -p $(@D)
-	erlc $(FILES)
-	erl -noshell -s test hello_world -s init stop
-	touch $@
+all:
+	rebar3 eunit
--- src/erlang99.erl
+++ src/erlang99.erl
@@ -1,16 +1,13 @@
 -module(erlang99).
+-include_lib("eunit/include/eunit.hrl").
 -export([last/1, penultimate/1]).
 
 last([H|[]]) -> H;
 last([_|T]) -> last(T).
 
-last_test() -> 
-  3 = last([1,2,3]).
+last_test_() -> 3 = last([1,2,3]).
 
 penultimate([H,_|[]]) -> H;
 penultimate([_|T]) -> penultimate(T).
 
-penultimate_test() -> 
-  2 = penultimate([1,2,3]).
-
-
+penultimate_test_() -> 2 = penultimate([1,2,3]).
