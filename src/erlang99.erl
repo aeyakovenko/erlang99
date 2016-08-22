895140975c73f7e229bc9eb4ee43ced1e3f6bc8b
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-08-22 16:27:30 -0700

    kth

--- src/erlang99.erl
+++ src/erlang99.erl
@@ -1,6 +1,9 @@
 -module(erlang99).
 -include_lib("eunit/include/eunit.hrl").
--export([last/1, penultimate/1]).
+-export([last/1
+        ,penultimate/1
+        ,kth/2
+        ]).
 
 last([H|[]]) -> H;
 last([_|T]) -> last(T).
@@ -11,3 +14,11 @@ penultimate([H,_|[]]) -> H;
 penultimate([_|T]) -> penultimate(T).
 
 penultimate_test_() -> ?_assertEqual(2, penultimate([1,2,3])).
+
+kth(0, [H|_]) -> H;
+kth(N, [_|T]) -> kth(N-1,T).
+
+kth_test_() ->[?_assertEqual(1, kth(0, [1,2,3]))
+              ,?_assertEqual(2, kth(1, [1,2,3]))
+              ,?_assertEqual(3, kth(2, [1,2,3]))
+              ].
