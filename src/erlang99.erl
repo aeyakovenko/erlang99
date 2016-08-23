bb4a8a6823ffc9051c549ad9f410a479f009d232
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-08-22 20:30:45 -0700

    more

--- src/erlang99.erl
+++ src/erlang99.erl
@@ -1,8 +1,10 @@
 -module(erlang99).
 -include_lib("eunit/include/eunit.hrl").
+-import(lists,[]).
 -export([last/1
         ,penultimate/1
         ,kth/2
+        ,reverse/1
         ]).
 
 last([H|[]]) -> H;
@@ -15,10 +17,22 @@ penultimate([_|T]) -> penultimate(T).
 
 penultimate_test_() -> ?_assertEqual(2, penultimate([1,2,3])).
 
-kth(0, [H|_]) -> H;
+kth(1, [H|_]) -> H;
 kth(N, [_|T]) -> kth(N-1,T).
 
-kth_test_() ->[?_assertEqual(1, kth(0, [1,2,3]))
-              ,?_assertEqual(2, kth(1, [1,2,3]))
-              ,?_assertEqual(3, kth(2, [1,2,3]))
-              ].
+kth_test_() -> [?_assertEqual(1, kth(1, [1]))
+               ,?_assertEqual(2, kth(2, [1,2]))
+               ,?_assertEqual(1, kth(1, [1,2,3]))
+               ,?_assertEqual(2, kth(2, [1,2,3]))
+               ,?_assertEqual(3, kth(3, [1,2,3]))
+               ].
+
+reverse(L) -> lists:foldl(fun(X,Z) -> [X|Z] end, [], L).
+
+reverse_test_() -> 
+    Ls=[[],[0],[0,1],[0,1,2]],
+    [?_assertEqual(lists:reverse(lists:nth(1,Ls)), reverse(lists:nth(1,Ls)))
+    ,?_assertEqual(lists:reverse(lists:nth(2,Ls)), reverse(lists:nth(2,Ls)))
+    ,?_assertEqual(lists:reverse(lists:nth(3,Ls)), reverse(lists:nth(3,Ls)))
+    ,?_assertEqual(lists:reverse(lists:nth(4,Ls)), reverse(lists:nth(4,Ls)))
+    ].
