c6c4dcbd2b3a47283ed5b3162982d9f95a944971
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-08-22 20:32:47 -0700

    spacing

--- src/erlang99.erl
+++ src/erlang99.erl
@@ -3,7 +3,7 @@
 -import(lists,[]).
 -export([last/1
         ,penultimate/1
-        ,kth/2
+        ,nth/2
         ,reverse/1
         ]).
 
@@ -17,22 +17,23 @@ penultimate([_|T]) -> penultimate(T).
 
 penultimate_test_() -> ?_assertEqual(2, penultimate([1,2,3])).
 
-kth(1, [H|_]) -> H;
-kth(N, [_|T]) -> kth(N-1,T).
+nth(1, [H|_]) -> H;
+nth(N, [_|T]) -> nth(N-1,T).
 
-kth_test_() -> [?_assertEqual(1, kth(1, [1]))
-               ,?_assertEqual(2, kth(2, [1,2]))
-               ,?_assertEqual(1, kth(1, [1,2,3]))
-               ,?_assertEqual(2, kth(2, [1,2,3]))
-               ,?_assertEqual(3, kth(3, [1,2,3]))
-               ].
+nth_test_() -> 
+  [?_assertEqual(1, nth(1, [1]))
+  ,?_assertEqual(2, nth(2, [1,2]))
+  ,?_assertEqual(1, nth(1, [1,2,3]))
+  ,?_assertEqual(2, nth(2, [1,2,3]))
+  ,?_assertEqual(3, nth(3, [1,2,3]))
+  ].
 
 reverse(L) -> lists:foldl(fun(X,Z) -> [X|Z] end, [], L).
 
 reverse_test_() -> 
-    Ls=[[],[0],[0,1],[0,1,2]],
-    [?_assertEqual(lists:reverse(lists:nth(1,Ls)), reverse(lists:nth(1,Ls)))
-    ,?_assertEqual(lists:reverse(lists:nth(2,Ls)), reverse(lists:nth(2,Ls)))
-    ,?_assertEqual(lists:reverse(lists:nth(3,Ls)), reverse(lists:nth(3,Ls)))
-    ,?_assertEqual(lists:reverse(lists:nth(4,Ls)), reverse(lists:nth(4,Ls)))
-    ].
+  Ls=[[],[0],[0,1],[0,1,2]],
+  [?_assertEqual(lists:reverse(lists:nth(1,Ls)), reverse(nth(1,Ls)))
+  ,?_assertEqual(lists:reverse(lists:nth(2,Ls)), reverse(nth(2,Ls)))
+  ,?_assertEqual(lists:reverse(lists:nth(3,Ls)), reverse(nth(3,Ls)))
+  ,?_assertEqual(lists:reverse(lists:nth(4,Ls)), reverse(nth(4,Ls)))
+  ].
